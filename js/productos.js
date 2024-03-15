1e28eee11028be7efaea37b4eaf087727b8e4c49
Benjamin Berardo Mazzalay <benja.berardo@gmail.com>
2024-03-15 03:35:55 -0300

    Segunda Pre-Entrega Javascript - Pagina de ventas

---
+++ js/productos.js
@@ -0,0 +1,161 @@
+let Teclados = [
+    {
+        Fabricante: "Corsair",
+        Modelo: "K70",
+        Precio: 85000,
+    },
+    {
+        Fabricante: "Redragon",
+        Modelo: "Kumara K552",
+        Precio: 65000,
+    },
+    {
+        Fabricante: "Noga",
+        Modelo: "Stormer Nkb-gk23",
+        Precio: 10000,
+    }
+]
+
+let Mouses = [
+    {
+        Fabricante: "Corsair",
+        Modelo: "M55",
+        Precio: 55000,
+    },
+    {
+        Fabricante: "Redragon",
+        Modelo: "Cobra M711",
+        Precio: 80000,
+    },
+    {
+        Fabricante: "Logitech",
+        Modelo: "Lightsync G203",
+        Precio: 40000,
+    }
+]
+
+let Auriculares = [
+    {
+        Fabricante: "Corsair",
+        Modelo: "VOID",
+        Precio: 200000,
+    },
+    {
+        Fabricante: "Redragon",
+        Modelo: "Zeus H510",
+        Precio: 90000,
+    },
+    {
+        Fabricante: "Redragon",
+        Modelo: "Hylas H260",
+        Precio: 45000,
+    }
+]
+
+let Monitores = [
+    {
+        Fabricante: "Samsung",
+        Modelo: '27" 75Hz',
+        Precio: 400000,
+    },
+    {
+        Fabricante: "HP",
+        Modelo: '27" 165Hz',
+        Precio: 370000,
+    },
+    {
+        Fabricante: "Philips",
+        Modelo: '19" 60Hz',
+        Precio: 120000,
+    }
+]
+
+let RAM = [
+    {
+        Fabricante: "Corsair",
+        Modelo: "32GB 3600MHz",
+        Precio: 180000,
+    },
+    {
+        Fabricante: "Kingston",
+        Modelo: "16GB 3200MHz",
+        Precio: 90000,
+    },
+    {
+        Fabricante: "Hikvision",
+        Modelo: "32GB 3200MHz",
+        Precio: 200000,
+    }
+]
+
+let Almacenamiento = [
+    {
+        Fabricante: "Western Digital",
+        Modelo: "Disco Duro 1TB",
+        Precio: 70000,
+    },
+    {
+        Fabricante: "Kingston",
+        Modelo: "Disco Solido 1TB",
+        Precio: 110000,
+    },
+    {
+        Fabricante: "Kingston",
+        Modelo: "Disco Solido M.2 1TB",
+        Precio: 85000,
+    }
+]
+
+let Graficas = [
+    {
+        Fabricante: "Nvidia Asus",
+        Modelo: "GTX 1650 4GB",
+        Precio: 300000,
+    },
+    {
+        Fabricante: "Nvidia MSI",
+        Modelo: "RTX 2060 Super 8GB",
+        Precio: 475000,
+    },
+    {
+        Fabricante: "Nvidia Zotac",
+        Modelo: "RTX 3060 12GB",
+        Precio: 700000,
+    }
+]
+
+let Motherboards = [
+    {
+        Fabricante: "Asus Rog",
+        Modelo: "B550-F AMD",
+        Precio: 325000,
+    },
+    {
+        Fabricante: "Asus Rog",
+        Modelo: "B650-e-e AMD",
+        Precio: 550000,
+    },
+    {
+        Fabricante: "Asus Rog",
+        Modelo: "Z790-f Intel",
+        Precio: 800000,
+    }
+]
+
+let Procesadores = [
+    {
+        Fabricante: "Intel",
+        Modelo: "Core i5-10400 10th GEN 6 Nucleos 4.3GHz",
+        Precio: 180000,
+    },
+    {
+        Fabricante: "Intel",
+        Modelo: "Core i3-10100F 10th GEN 4 Nucleos 4.3GHz",
+        Precio: 115000,
+    },
+    {
+        Fabricante: "Intel",
+        Modelo: "Core i7-10700 10th GEN 8 Nucleos 4.8GHz",
+        Precio: 370000,
+    }
+]
